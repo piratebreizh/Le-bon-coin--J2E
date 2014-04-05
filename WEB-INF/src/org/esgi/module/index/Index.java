564744fc0f26a6771d097c1e9af7465bad8d5e41
antoineludwig <antoine.ludwig60@gmail.com>
2014-04-05 23:32:59 +0200

    Version propre après pull

--- WEB-INF/src/org/esgi/module/index/Index.java
+++ WEB-INF/src/org/esgi/module/index/Index.java
@@ -3,19 +3,18 @@ package org.esgi.module.index;
 import org.esgi.web.action.AbstractAction;
 import org.esgi.web.action.IContext;
 
-public class Index extends AbstractAction{
-	
+public class Index extends AbstractAction {
+
 	public void execute(IContext context) throws Exception {
-		context.getVelocityContext().put("title", "Index");
-		System.out.println(context.getRequest().getParameter("login"));
-	}
-	@Override
-	public String getRoute() {
-		return "^/$";
 		context.getVelocityContext().put("title", "Bienvenue sur LeBonCoinJ2E");
 
-		context.addCSSDependency(context.getProperties().get("context") + "/res/css/commun.css");
+		context.addCSSDependency(context.getProperties().get("context")
+				+ "/res/css/commun.css");
 	}
 
+	@Override
+	public String getRoute() {
+		return "/index/";
+	}
 
 }
