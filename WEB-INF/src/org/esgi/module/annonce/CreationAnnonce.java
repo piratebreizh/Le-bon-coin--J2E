376332e23d8fd16cff4aad78bc3bf79b40ef83ee
piratebreizh <alexandre.d.bzh@gmail.com>
2014-05-14 20:30:56 +0200

    Modification de la création des annonces, l'utilisateur est maintenant enregistré en BDD et la création ne peux pas se faire si l'utilisateur n'est pas connecté

--- WEB-INF/src/org/esgi/module/annonce/CreationAnnonce.java
+++ WEB-INF/src/org/esgi/module/annonce/CreationAnnonce.java
@@ -7,32 +7,38 @@ import org.esgi.web.action.AbstractAction;
 import org.esgi.web.action.IContext;
 import org.esgi.orm.ORM;
 import org.esgi.orm.model.Annonce;
+import org.esgi.orm.model.User;
 
 public class CreationAnnonce extends AbstractAction {
 
 	public void execute(IContext context) throws Exception {
 		context.getVelocityContext().put("title", "Creation d'une annonce");
 
-	
-
-		if(context.getRequest().getParameter("region") != null && context.getRequest().getParameter("description") != null && 
-				context.getRequest().getParameter("titre") != null && context.getRequest().getParameter("categorie") != null 
-				&& context.getRequest().getParameter("prix") != null ){
-			
-			
-			
-			// A COMPLETER PAS FINI IL FAUT RECUPERER LE NUMERO D'UTILISATEUR QUI EST CONNECTER
-			java.util.Date today = new java.util.Date();
-			Date dateDuJouDate = new Date(today.getTime());
-			Annonce nouvelleCreation = new Annonce(context.getRequest().getParameter("titre"),context.getRequest().getParameter("description"),
-					1,context.getRequest().getParameter("region"),context.getRequest().getParameter("categorie"),Double.parseDouble(context.getRequest().getParameter("prix")),
-					dateDuJouDate);
-			
-			ORM.save(nouvelleCreation);
-			
-			
-		}
 		
+		if(context.getProperties().get("userConnected") == null){
+			context.getVelocityContext().put("utilisateurConnecter", false);		
+		}else{
+			
+			User userConnected = (User) context.getProperties().get("userConnected");
+			context.getVelocityContext().put("utilisateurConnecter", true);
+
+			if(context.getRequest().getParameter("region") != null && context.getRequest().getParameter("description") != null && 
+					context.getRequest().getParameter("titre") != null && context.getRequest().getParameter("categorie") != null 
+					&& context.getRequest().getParameter("prix") != null ){
+				
+				
+				
+				java.util.Date today = new java.util.Date();
+				Date dateDuJouDate = new Date(today.getTime());
+				Annonce nouvelleCreation = new Annonce(context.getRequest().getParameter("titre"),context.getRequest().getParameter("description"),
+						userConnected.getId(),context.getRequest().getParameter("region"),context.getRequest().getParameter("categorie"),Double.parseDouble(context.getRequest().getParameter("prix")),
+						dateDuJouDate);
+				
+				ORM.save(nouvelleCreation);
+				
+				
+			}
+		}
 		
 		context.addCSSDependency(context.getProperties().get("context")
 				+ "/res/css/commun.css");/*
